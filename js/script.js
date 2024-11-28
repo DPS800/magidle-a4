81215ea1c04232a40b480933a2f4b2a356ea085f
DPS800 <deepzgrewalz@gmail.com>
2024-11-28 20:47:12 +0000

    Fixing RNG

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 // Tooltip script
-$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();});
+$(document).ready(function () { $('[data-toggle="tooltip"]').tooltip(); });
 
 // RNG
-var link = document.getElementById('getNumber');
-onclick = getNumber;
-
-function getNumber() {
-    var minNumber = 100;
-    var maxNumber = 250;
-    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber);
-    $('#myNumber').html(randomnumber);
-    return false;
+function random() {
+    document.getElementById("myNum").innerHTML = Math.floor(Math.random() * (250 - 100) + 100);
 }
