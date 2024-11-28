6f496c661954087cc749d34b7aa2976216ce52d4
DPS800 <deepzgrewalz@gmail.com>
2024-11-28 19:50:37 +0000

    Adding training modal with RNG

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 // Tooltip script
 $(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();});
+
+// RNG
+var link = document.getElementById('getNumber');
+onclick = getNumber;
+
+function getNumber() {
+    var minNumber = 100;
+    var maxNumber = 250;
+    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber);
+    $('#myNumber').html(randomnumber);
+    return false;
+}
